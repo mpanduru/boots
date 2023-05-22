fbe1829770e5f3f155bd40e46e9621194299edc8
mpanduru <mpanduru@cloudbasesolutions.comm>
2023-05-22 08:54:27 +0000

    Revert "test1"
    This reverts commit 7800cbbea171282382e640ae9cff76c905768d9b.

--- cmd/boots/main_test.go
+++ cmd/boots/main_test.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
-	"github.com/mpanduru/ipxedust"
+	"github.com/tinkerbell/ipxedust"
 )
 
 func TestParser(t *testing.T) {
